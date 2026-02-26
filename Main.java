d7ed5edea24c823a9f4c3ff516dbe98c065da823
roudrikhorne-NOAA <roudrik.horne@noaa.gov>
2026-02-26 14:45:03 -0800

    Create Main.java
    Created ny first Java file

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is your first line of code!
+        System.out.println("Hello Guru! My development environment is live.");
+        
+        // Let's do a little math to prove it's working
+        int result = 5 + 5;
+        System.out.println("The result of 5 + 5 is: " + result);
+    }
+}
